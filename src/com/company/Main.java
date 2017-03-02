d29ef0d2ed498af1e70fa7d3457a7c2ca50eb3dd
Dr. Auxin <paul.torrillo@gmail.com>
2017-03-02 10:32:53 -0500

    hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,29 +8,30 @@ public class Main {
         Scanner user = new Scanner(new File("MarchData"));
         ArrayList<Team> madness = new ArrayList();
         int count = 1;
+        String storage = "";
         boolean add = true;
         String year = "0";
         int index = 0;
-        while (!user.hasNextLine()) {
+        while (user.hasNextLine()) {
             if (count % 9 == 1) {
                 year = user.nextLine();
                 count++;
             }
-            if (count % 9 == 2)
-                count++;
-            if (count % 9 == 3) {
+
+            if (count % 9 == 2) {
+                storage = user.nextLine();
                 if (madness.isEmpty()) {
-                    madness.add(new Team(user.nextLine()));
+                    madness.add(new Team(storage));
                     madness.get(madness.size() - 1).score = madness.get(madness.size() - 1).score + Integer.parseInt(year) + 4 - 1938;
                     count++;
                 } else {
                     for (int a = 0; a < madness.size(); a++)
-                        if (madness.get(a).id.equalsIgnoreCase(user.nextLine())) {
+                        if (madness.get(a).id.equalsIgnoreCase(storage)) {
                             index = a;
                             add = false;
                         }
                     if (add) {
-                        madness.add(new Team(user.nextLine()));
+                        madness.add(new Team(storage));
                         madness.get(madness.size() - 1).score = madness.get(madness.size() - 1).score + Integer.parseInt(year) + 4 - 1938;
                         count++;
                     } else {
@@ -40,75 +41,97 @@ public class Main {
                     }
                 }
             }
-                if (count % 9 == 4) {
-                    if (madness.isEmpty()) {
-                        madness.add(new Team(user.nextLine()));
+            if (count % 9 == 3) {
+                user.nextLine();
+                count++;
+            }
+            if (count % 9 == 4) {
+                storage = user.nextLine();
+                if (madness.isEmpty()) {
+                    madness.add(new Team(storage));
+                    madness.get(madness.size() - 1).score = madness.get(madness.size() - 1).score + Integer.parseInt(year) + 3 - 1938;
+                    count++;
+                } else {
+                    for (int a = 0; a < madness.size(); a++)
+                        if (madness.get(a).id.equalsIgnoreCase(storage)) {
+                            index = a;
+                            add = false;
+                        }
+                    if (add) {
+                        madness.add(new Team(storage));
                         madness.get(madness.size() - 1).score = madness.get(madness.size() - 1).score + Integer.parseInt(year) + 3 - 1938;
                         count++;
                     } else {
-                        for (int a = 0; a < madness.size(); a++)
-                            if (madness.get(a).id.equalsIgnoreCase(user.nextLine())) {
-                                index = a;
-                                add = false;
-                            }
-                        if (add) {
-                            madness.add(new Team(user.nextLine()));
-                            madness.get(madness.size() - 1).score = madness.get(madness.size() - 1).score + Integer.parseInt(year) + 3 - 1938;
-                            count++;
-                        } else {
-                            madness.get(index).score = madness.get(index).score + Integer.parseInt(year) + 3 - 1938;
-                            add = true;
-                            count++;
-                        }
+                        madness.get(index).score = madness.get(index).score + Integer.parseInt(year) + 3 - 1938;
+                        add = true;
+                        count++;
                     }
                 }
+            }
 
-                if (count % 9 == 5) {
-                    if (madness.isEmpty()) {
-                        madness.add(new Team(user.nextLine()));
+            if (count % 9 == 5) {
+                storage = user.nextLine();
+                if (madness.isEmpty()) {
+                    madness.add(new Team(storage));
+                    madness.get(madness.size() - 1).score = madness.get(madness.size() - 1).score + Integer.parseInt(year) + 2 - 1938;
+                    count++;
+                } else {
+                    for (int a = 0; a < madness.size(); a++)
+                        if (madness.get(a).id.equalsIgnoreCase(storage)) {
+                            index = a;
+                            add = false;
+                        }
+                    if (add) {
+                        madness.add(new Team(storage));
                         madness.get(madness.size() - 1).score = madness.get(madness.size() - 1).score + Integer.parseInt(year) + 2 - 1938;
                         count++;
                     } else {
-                        for (int a = 0; a < madness.size(); a++)
-                            if (madness.get(a).id.equalsIgnoreCase(user.nextLine())) {
-                                index = a;
-                                add = false;
-                            }
-                        if (add) {
-                            madness.add(new Team(user.nextLine()));
-                            madness.get(madness.size() - 1).score = madness.get(madness.size() - 1).score + Integer.parseInt(year) + 2 - 1938;
-                            count++;
-                        } else {
-                            madness.get(index).score = madness.get(index).score + Integer.parseInt(year) + 2 - 1938;
-                            add = true;
-                            count++;
-                        }
+                        madness.get(index).score = madness.get(index).score + Integer.parseInt(year) + 2 - 1938;
+                        add = true;
+                        count++;
                     }
                 }
-                if (count % 9 == 6) {
-                    if (madness.isEmpty()) {
-                        madness.add(new Team(user.nextLine()));
+            }
+            if (count % 9 == 6) {
+                storage = user.nextLine();
+                if (madness.isEmpty()) {
+                    madness.add(new Team(storage));
+                    madness.get(madness.size() - 1).score = madness.get(madness.size() - 1).score + Integer.parseInt(year) + 1 - 1938;
+                    count++;
+                } else {
+                    for (int a = 0; a < madness.size(); a++)
+                        if (madness.get(a).id.equalsIgnoreCase(storage)) {
+                            index = a;
+                            add = false;
+                        }
+                    if (add) {
+                        madness.add(new Team(storage));
                         madness.get(madness.size() - 1).score = madness.get(madness.size() - 1).score + Integer.parseInt(year) + 1 - 1938;
                         count++;
                     } else {
-                        for (int a = 0; a < madness.size(); a++)
-                            if (madness.get(a).id.equalsIgnoreCase(user.nextLine())) {
-                                index = a;
-                                add = false;
-                            }
-                        if (add) {
-                            madness.add(new Team(user.nextLine()));
-                            madness.get(madness.size() - 1).score = madness.get(madness.size() - 1).score + Integer.parseInt(year) + 1 - 1938;
-                            count++;
-                        } else {
-                            madness.get(index).score = madness.get(index).score + Integer.parseInt(year) + 1 - 1938;
-                            add = true;
-                            count++;
-                        }
+                        madness.get(index).score = madness.get(index).score + Integer.parseInt(year) + 1 - 1938;
+                        add = true;
+                        count++;
                     }
                 }
-                if (count)
+            }
+            if (count % 9 == 7) {
+                user.nextLine();
+                count++;
+            }
+            if (count % 9 == 8) {
+                user.nextLine();
+                count++;
+            }
+            if (count % 9 == 0) {
+                user.nextLine();
+                count++;
             }
         }
+        System.out.println(madness.get(0).id + madness.get(0).score);
+        System.out.println(madness.get(1).id + madness.get(1).score);
+        System.out.println(madness.get(2).id + madness.get(2).score);
+        System.out.println(madness.get(3).id + madness.get(3).score);
     }
+}
 
